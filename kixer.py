1e2cb41f23f8922b72a5d9f9f1459bfc4e104cb8
ankitkr23835 <72962795+ankitkr23835@users.noreply.github.com>
2021-09-08 15:04:12 +0530

    Create kixer.py

---
+++ kixer.py
@@ -0,0 +1,13 @@
+xed=input('your file name:')
+dex=int(input('your character size:'))
+a="abcdefghijklmnopqrstuvwxyz1234567890!@#$%^,:;?/_-(){}%"
+for z in a:
+	for y in a:
+		for x in a:
+			for w in a:
+				if dex ==1
+					print(z, file=open(xed, "a"))
+				elif dex == 2:
+					print(z,y, sep="", file=open(xed, "a"))
+				elif dex ==3:
+					print(z,y,x, sep="", file=open(xed, "a"))
